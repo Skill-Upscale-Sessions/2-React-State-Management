96956f6e0cd1771b15226acb5bd557da922aeedb
Shanuka Lakshan <89212510+ShanukaLakshan@users.noreply.github.com>
2024-11-08 18:38:07 +0530

    Implement ThemeContext for theme management and enhance UseContextHookComponent with toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import ReactHooks from "./components/ReactHooks";
 
+export const ThemeContext = createContext({
+  theme: "dark",
+  setTheme: (theme: string) => {},
+});
+
 const App = () => {
+  const [theme, setTheme] = useState("dark");
+
   return (
-    <Provider store={store}>
-      <div className=" bg-gray-50 p-6 flex justify-center items-center">
-        <div className="max-w-lg w-full bg-white shadow-lg rounded-lg p-8">
-          <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
-            Learn React
-          </h1>
-          <ReactHooks />
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Provider store={store}>
+        <div className=" bg-gray-50 p-6 flex justify-center items-center">
+          <div className="max-w-lg w-full bg-white shadow-lg rounded-lg p-8">
+            <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
+              Learn React
+            </h1>
+            <ReactHooks />
+          </div>
         </div>
-      </div>
-    </Provider>
+      </Provider>
+    </ThemeContext.Provider>
   );
 };
 
--- src/components/UseContextHookComponent.tsx
+++ src/components/UseContextHookComponent.tsx
@@ -1,11 +1,41 @@
+import { useContext } from "react";
+import { ThemeContext } from "../App";
+import { Flex, Heading, Button } from "@radix-ui/themes";
+
 const UseContextHookComponent = ({
   isUseContextShow,
 }: {
   isUseContextShow: boolean;
 }) => {
+  const themeContext = useContext(ThemeContext);
+
+  const isDarkTheme = themeContext.theme === "dark";
+
   if (isUseContextShow) {
-    return <div>UseContextHookComponent</div>;
+    return (
+      <Flex
+        direction="column"
+        gap="4"
+        align="center"
+        justify="center"
+        className={`mt-4 p-4 ${isDarkTheme ? "bg-gray-800" : "bg-gray-100"}`}
+      >
+        <Heading size="6" className={isDarkTheme ? "text-white" : "text-black"}>
+          useContext Hook
+        </Heading>
+
+        <Button
+          className={`${
+            isDarkTheme ? "bg-blue-700" : "bg-blue-500"
+          } text-white`}
+          onClick={() => themeContext.setTheme(isDarkTheme ? "light" : "dark")}
+        >
+          Toggle Theme
+        </Button>
+      </Flex>
+    );
   }
+
   return null;
 };
 
