1c389d2e52b8f8fbf29279e285bea32ca98c2dcb
Shanuka Lakshan <89212510+ShanukaLakshan@users.noreply.github.com>
2024-11-08 15:34:41 +0530

    Enhance UseEffectHookComponent with state management for number and square, and integrate Radix UI for improved UI elements

--- src/components/UseEffectHookComponent.tsx
+++ src/components/UseEffectHookComponent.tsx
@@ -1,13 +1,51 @@
-import React from "react";
+import { Button, Flex, Heading } from "@radix-ui/themes";
+import React, { useEffect, useState } from "react";
 
 const UseEffectHookComponent = ({
   isUseEffectShow,
 }: {
   isUseEffectShow: boolean;
 }) => {
+  const [number, setNumber] = useState(0);
+  const [square, setSquare] = useState(0);
+
+  useEffect(() => {
+    setSquare(number * number);
+  }, [number]);
+
   if (isUseEffectShow) {
-    return <div>UseEffectHookComponent</div>;
+    return (
+      <Flex
+        direction="column"
+        gap="4"
+        align="center"
+        justify="center"
+        className="p-4 space-x-4 bg-blue-100"
+      >
+        <Heading size="6">useEffect Hook</Heading>
+
+        <div className="text-lg font-semibold">Number: {number}</div>
+        <Button
+          onClick={() => setNumber(number + 1)}
+          variant="surface"
+          color="green"
+        >
+          Increment
+        </Button>
+        <Button
+          onClick={() => setNumber(number - 1)}
+          variant="surface"
+          color="red"
+        >
+          Decrement
+        </Button>
+
+        <div className="text-lg font-semibold">Square: {square}</div>
+      </Flex>
+    );
   }
+
+  return null;
 };
 
 export default UseEffectHookComponent;
