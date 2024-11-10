59b690ebd2a42fca2d52c38077a0c68f12754943
Shanuka Lakshan <89212510+ShanukaLakshan@users.noreply.github.com>
2024-11-10 19:28:51 +0530

    Update title in ReactHooks component: change text to 'Learn React Hooks'

--- src/components/common/ReactHooks.tsx
+++ src/components/common/ReactHooks.tsx
@@ -58,7 +58,7 @@ const ReactHooks: React.FC = () => {
       <div className=" bg-gray-50 p-6 flex justify-center items-center">
         <div className="max-w-lg w-full bg-white shadow-lg rounded-lg p-8">
           <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
-            Learn React
+            Learn React Hooks
           </h1>
           <Flex direction="column" gap="4" className="mb-5">
             <Badge color="ruby" className="text-lg mb-2">
